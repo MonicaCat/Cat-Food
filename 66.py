5c78a39c46cdee712db46797984b141354591961
MonicaCat <carolynxiao@qq.com>
2019-05-16 01:45:54 +0800

    求幂函数、指数函数(pow)，对数函数(log)，三角函数(sin, cos, tan)

---
+++ 66.py
@@ -0,0 +1,12 @@
+import math
+print(math.pow(3,4))
+
+print(math.pow(3,3.1))
+
+print(math.log(math.e))
+print(math.log2(4))
+print(math.log10(100))
+print(math.sin(math.pi/2))
+print(math.cos(math.pi))
+print(math.tan(math.pi/2))
+print(math.tan(math.pi/4))
